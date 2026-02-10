21b36fcb260e6cef593059bcc7098e8fe8c7e58a
Janakesh <janakeshvs@gmail.com>
2026-02-10 11:01:13 +0530

    Janakesh

--- music player.py
+++ music player.py
@@ -1,1 +1,27 @@
-code
+#part 2
+
+
+class SongNode:
+    def __init__(self, title, artist, duration):
+        self.title = title
+        self.artist = artist
+        self.duration = duration
+        self.prev = None
+        self.next = None
+
+
+class Playlist:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.current = None
+
+    def add_song(self, title, artist, duration):
+        new_song = SongNode(title, artist, duration)
+
+        if self.head is None:
+            self.head = self.tail = self.current = new_song
+        else:
+            self.tail.next = new_song
+            new_song.prev = self.tail
+            self.tail = new_song
